9479dff8fe5e668093e515fe6524849ef48d487b
liorspencer <juliodiversity@gmail.com>
2024-10-09 21:22:15 -0300

    adicionado variável checagem (para testes)

--- app/src/main/java/com/overflow/infinitystore/MainActivity.java
+++ app/src/main/java/com/overflow/infinitystore/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    String checagem = "ok";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
